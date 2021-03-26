b3d4b121398636e0f5094c73b45f47bab5abbc52
Дмитрий <dvr07@yandex.ru>
2021-03-26 19:11:50 +0300

    Create component Car

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+import Car from './Car/Car';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        const divStyle = {
+            textAlign: 'center'
+        }
+        return ( <
+            div style = { divStyle } >
+            <
+            h1 > Hello world < /h1>
+
+            <
+            Car / >
+            <
+            /div>
+        );
+    }
 }
 
 export default App;
