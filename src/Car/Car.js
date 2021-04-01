3c48126e73001ec2d353b2bc65e7b71991b8238f
Дмитрий <dvr07@yandex.ru>
2021-04-01 12:56:37 +0300

    adding bind

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default (props) => ( <
-    div > < h3 > Car name: { props.name } < /h3>  <
-    p > YEAR: < strong > { props.year } < /strong></p > { props.children } < /div>
+    div >
+    <
+    h3 > Car name: { props.name } < /h3>   <
+    p > YEAR: < strong > { props.year } < /strong></p > { props.children } <
+    button onClick = { props.onChangeTitle } > Click < /button> <
+    /div>
 )
